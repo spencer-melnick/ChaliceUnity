39a3363d438cefb7fdeb2e0c687c0fd65f8413b8
Spencer Melnick <smelnick97@gmail.com>
2019-05-03 00:36:02 -0400

    Fixed jitter caused by using Camera.current instead of Camera.main in PlayerController; cleaned up PlayerController and Character

---
+++ Assets/Scripts/Cameras/OrbitingCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitingCamera : MonoBehaviour
+{
+    public GameObject target;
+    public Vector3 targetOffset = Vector3.zero;
+    public Vector3 cameraOffset = new Vector3(0.0f, 0.0f, -10.0f);
+    public Vector3 orbitAngles = new Vector3(40.0f, 0.0f, 0.0f);
+
+    void Update()
+    {
+        Vector3 targetPosition = target.transform.position + this.targetOffset;
+        Quaternion rotation = Quaternion.Euler(orbitAngles);
+        Vector3 cameraPosition = targetPosition + rotation * this.cameraOffset;
+        this.transform.SetPositionAndRotation(cameraPosition, rotation);
+    }
+}
--- Assets/Scripts/Players/BasicPlayerController.cs
+++ Assets/Scripts/Players/BasicPlayerController.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class BasicPlayerController : MonoBehaviour
 {
 
-    public Character character;
-    public GameObject cameraObject;
+    public Character controlledCharacter;
+    public OrbitingCamera controlledCamera;
+
     public float horizontalRotationSpeed = 150.0f;
     public float verticalRotationSpeed = 200.0f;
     public float maxCameraPitch = 80.0f;
@@ -19,29 +20,57 @@ public class BasicPlayerController : MonoBehaviour
 
     private void Update()
     {
-        float horizontalMovement = Input.GetAxisRaw("Horizontal");
-        float verticalMovement = Input.GetAxisRaw("Vertical");
-        Vector3 movement = new Vector3(horizontalMovement, 0.0f, verticalMovement);
-        movement.Normalize();
+        RotateCamera();
+        MoveCharacter();
+    }
 
-        // TODO: Check to make sure camera is active!
-        float horizontalRotation = Input.GetAxisRaw("Mouse X") * horizontalRotationSpeed * Time.deltaTime;;
-        float verticalRotation = Input.GetAxisRaw("Mouse Y") * verticalRotationSpeed * Time.deltaTime;
-        Vector3 cameraRotation = cameraObject.transform.eulerAngles;
-        cameraRotation.y += horizontalRotation;
-        cameraRotation.x -= verticalRotation;
+    private void LateUpdate()
+    {
+        // TODO: Resolve collisions
+    }
 
-        if (cameraRotation.x > 180.0f)
+    void RotateCamera()
+    {
+        // Check if the controlled camera is active
+        if (controlledCamera != null && controlledCamera.GetComponentInParent<Camera>() == Camera.main)
         {
-            cameraRotation.x -= 360.0f;
+            // Scale mouse movement by delta time and appropriate speed
+            float horizontalRotation = Input.GetAxisRaw("Mouse X") * horizontalRotationSpeed * Time.deltaTime; ;
+            float verticalRotation = Input.GetAxisRaw("Mouse Y") * verticalRotationSpeed * Time.deltaTime;
+
+            // Operate along Euler angles
+            Vector3 cameraAngles = controlledCamera.orbitAngles;
+            cameraAngles.y += horizontalRotation;
+            cameraAngles.x -= verticalRotation;
+
+            // Check for angle wrap-around to allow for proper clamping
+            if (cameraAngles.x > 180.0f)
+            {
+                cameraAngles.x -= 360.0f;
+            }
+
+            // Clamp angles to limits
+            cameraAngles.y %= 360.0f;
+            cameraAngles.x = Mathf.Clamp(cameraAngles.x, minCameraPitch, maxCameraPitch);
+            controlledCamera.orbitAngles = cameraAngles;
         }
+    }
 
-        cameraRotation.x = Mathf.Clamp(cameraRotation.x, minCameraPitch, maxCameraPitch);
-        cameraObject.transform.SetPositionAndRotation(character.transform.position, Quaternion.Euler(cameraRotation));
+    void MoveCharacter()
+    {
+        if (controlledCharacter != null)
+        {
+            // Normalize axis inputs along xz plane
+            float horizontalMovement = Input.GetAxisRaw("Horizontal");
+            float verticalMovement = Input.GetAxisRaw("Vertical");
+            Vector3 movement = new Vector3(horizontalMovement, 0.0f, verticalMovement);
+            movement.Normalize();
 
-        Quaternion cameraDirection = Camera.current.transform.rotation;
-        Quaternion lookDirection = Quaternion.Euler(0.0f, cameraDirection.eulerAngles.y, 0.0f);
-        movement = lookDirection * movement;
-        character.Move(movement);
+            // Move relatve to camera
+            Quaternion cameraDirection = Camera.main.transform.rotation;
+            Quaternion lookDirection = Quaternion.Euler(0.0f, cameraDirection.eulerAngles.y, 0.0f);
+            movement = lookDirection * movement;
+            controlledCharacter.Move(movement);
+        }
     }
 }
